13caa1ef03bf39fb51934faa1c4814849508ad2d
Shae Erisson <shae@scannedinavian.com>
2025-06-11 16:17:47 -0400

    add some things

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,81 @@
 pub mod lib {
+
+    use binrw::*;
     use std::io;
     use std::io::Write;
+
+    #[derive(BinRead, PartialEq, PartialOrd, Debug)]
+    pub struct Share {
+	pub lease_version: u32,
+	lease_data_length: u32,
+	lease_count: u32,
+	share_version: u32,
+	block_size: u32,
+	data_size: u32,
+	data_offset: u32,
+	plaintxt_hash_tree_offset: u32,
+	cryptxt_hash_tree_offset: u32,
+	block_hashes_offset: u32,
+	share_hashes_offset: u32,
+	uri_ext_offset: u32,
+	// data starts now!
+	#[br(big, count = data_size)]
+	share_data: Vec<u8>,
+    }
+
+    // use nom::*;
+    /*
+    struct UriExtension {
+    /// codec_name must be "crs" as of 2025
+    codec_name: String,
+    /// segment size, needed shares, total shares
+    codec_params: (usize, usize, usize),
+    /// segment size, needed shares, total shares
+    tail_codec_params: (usize, usize, usize),
+    /// application size in bytes
+    size: usize,
+    /// segment size in bytes
+    segment_size: usize,
+    segment_count: usize,
+    share_count_required: usize,
+    share_count_total: usize,
+    crypt_text_hash: Vec<u8>,
+    crypt_text_root_hash: Vec<u8>,
+    share_root_hash: Vec<u8>,
+    }
+    */
+
+    // demo input "codec_name:3:crs,codec_params:5:8-1-2,"
+    /* guess:
+    split on commas for each "value"
+    split on colons inside the "value"
+
+    Wait, no... the hashes are binary values, and thus could have a ',' in their value, so we have to continue with binary parsing!
+    That is, we must read from the start of the URI extension block up to the number between two colons, and then consume that many bytes for this named value.
+    We cannot split on commas because the hashes could contain a comma!
+    */
+
+    struct UebValue {
+	name: String,
+	byte_count: usize,
+	value: Vec<u8>,
+    }
+    /*
+      What about using the names as "magic numbers" as found in the first few bytes of a file?
+    */
+
+    // #[derive(BinRead)]
+    // enum URI_Extension {
+    //     #[brw(big, magic = b"share_root_hash:")]
+    //     Share_root_hash { Vec<u8> },
+    //     #[br(magic(0u8))] Rect {
+    //	left: i16, top: i16, right: i16, bottom: i16
+    //     },
+    //     #[br(magic(1u8))]
+    //     Oval { origin: Point, rx: u8, ry: u8 }
+    // }
 
     pub fn print_hello_world() {
 	let _ = io::stdout().write_all(b"Hello, world!\n");
     }
 }
-pub mod uri_extension;
--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,14 @@
+use crate::io::Cursor;
 use binrw::io::*;
-use binrw::*;
+use binrw::BinReaderExt;
+use binrw_tahoe::lib::*;
 use std::fs::File;
 use std::io;
 use std::result;
 // this works for files small enough to fit into v1 ? We hope?
-#[derive(BinRead, PartialEq, PartialOrd, Debug)]
-struct Share {
-    lease_version: u32,
-    lease_data_length: u32,
-    lease_count: u32,
-    share_version: u32,
-    block_size: u32,
-    data_size: u32,
-    data_offset: u32,
-    plaintxt_hash_tree_offset: u32,
-    cryptxt_hash_tree_offset: u32,
-    block_hashes_offset: u32,
-    share_hashes_offset: u32,
-    uri_ext_offset: u32,
-    // data starts now!
-    // #[br(big, count = data_size)]
-    // share_data: Vec<u8>,
-}
+
 fn main() -> result::Result<(), io::Error> {
-    {
-        let mut part1 = File::open("1of2.0")?;
-        let mut pile_of_bytes: Vec<u8> = vec![0; 2500];
-        part1.read(&mut pile_of_bytes).unwrap();
-        let mut rdr = Cursor::new(pile_of_bytes);
-        let _: Share = dbg!(rdr.read_be().unwrap());
-    }
+    let _ = read_cap("1of2.0");
     let mut part2 = File::open("1of2.1")?;
     let mut pile_of_bytes: Vec<u8> = vec![0; 2500];
     part2.read(&mut pile_of_bytes).unwrap();
@@ -37,4 +16,26 @@ fn main() -> result::Result<(), io::Error> {
     let _: Share = dbg!(rdr.read_be().unwrap());
 
     Ok(())
+}
+
+fn read_cap(filename: &str) -> result::Result<Share, io::Error> {
+    let mut part1 = File::open(filename)?;
+    let mut pile_of_bytes: Vec<u8> = vec![0; 2500];
+    part1.read(&mut pile_of_bytes).unwrap();
+    let mut rdr = Cursor::new(pile_of_bytes);
+    let share: Share = rdr.read_be().unwrap();
+    Ok(share)
+}
+
+#[cfg(test)]
+mod tests {
+    // for 1of2.0 and 1of2.1 :
+    // wellKnownConvergenceSecret = decodeBase32Unpadded "lcngfrvgaksfwrelc6ae5kucb3zufssoe6cj74rozcqibnl6uy2a"
+    // cap = "URI:CHK:pyv3qypbpk6knq5ozeibenuubq:jh3twlgmxtytwqtzn6jtbsfy2w574ybkcnalurlnlq2snuu3j5da:1:2:56"
+    use super::*;
+    #[test]
+    fn it_works() {
+	let s = read_cap("1of2.0").unwrap();
+	assert_eq!(s.lease_version, 2);
+    }
 }
